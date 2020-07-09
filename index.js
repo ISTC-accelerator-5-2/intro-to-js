c536ee4075ae4b98a783703327abad835af86d38
narghev <narghev@protonmail.com>
2020-07-09 13:31:39 +0400

    classwork

---
+++ index.js
@@ -0,0 +1,46 @@
+// math operator
+
+// comment
+/* 
+  multi line comment
+*/
+
+// console.log(5+6); // addition operator
+// console.log(7-6); // substraction operator
+// console.log(5*6); // multiplication
+// console.log(8/2); // division
+// console.log(8%2); // mod operator
+// console.log(2 ** 7); // exp operator
+
+// console.log(11/10);
+// console.log(0/10);
+// console.log(10/0);
+// console.log(Infinity + 5);
+// console.log(-10/0);
+// console.log(0.2 + 0.1);
+// console.log(0.2 * 5);
+// console.log((1 + 5) * 6);
+// console.log(5 % 1 + 4);
+// console.log(1.2 * 6 % 5);
+
+// Variables
+var n = 10;
+// camelCase
+var myVariable = 0;
+let x = 1;
+let u;
+const y = 10;
+
+// console.log(myVariable + x);
+
+console.log(myVariable);
+myVariable = 2;
+console.log(myVariable);
+
+console.log(x);
+x = 'hello';
+console.log(x);
+
+console.log(y);
+y = 8;
+console.log(y);
